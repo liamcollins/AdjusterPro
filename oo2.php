19b5d7ac5f0b2f7a38aef8db98f65471bba188c6
Liam Collins <1mailliam1@gmail.com>
2010-09-11 15:28:29 -0700

    Added oo2.php

---
+++ oo2.php
@@ -0,0 +1,38 @@
+<?php
+class Foo
+{
+    public static $my_static = 'foo';
+    public static $my_static2 = 'another static';
+
+    public function staticValue() {
+        return self::$my_static;
+    }
+}
+
+class Bar extends Foo
+{
+    public function fooStatic() {
+        return parent::$my_static;
+    }
+}
+
+
+print Foo::$my_static . "\n";
+
+$foo = new Foo();
+print $foo->staticValue() . "\n";
+print $foo->my_static . "\n";      // Undefined "Property" my_static 
+
+print $foo::$my_static . "\n";
+$classname = 'Foo';
+print $classname::$my_static . "\n"; // As of PHP 5.3.0
+
+print Bar::$my_static . "\n";
+$bar = new Bar();
+print $bar->fooStatic() . "\n";
+$foo2 = new Bar();
+print $foo2->staticValue() . "new stuff \n";
+print Bar::$my_static2;
+
+?>
+
